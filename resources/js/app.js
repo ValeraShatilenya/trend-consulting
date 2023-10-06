6ed8b193e8a221e24e622907efdc6543443aec7a
Valery <v.shatsilenia@akdev.by>
2023-10-06 11:54:14 +0300

    Add inertia to front

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,16 @@
-import './bootstrap';
+import "./bootstrap";
+
+import { createApp, h } from "vue";
+import { createInertiaApp } from "@inertiajs/vue3";
+
+createInertiaApp({
+    resolve: (name) =>
+        import.meta.glob("./Pages/**/*.vue", { eager: true })[
+            `./Pages/${name}.vue`
+        ],
+    setup({ el, App, props, plugin }) {
+        createApp({ render: () => h(App, props) })
+            .use(plugin)
+            .mount(el);
+    },
+});
