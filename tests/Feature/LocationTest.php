886dae245269c6b39b4e10b6e8f7c43a60ffb571
Valery <v.shatsilenia@akdev.by>
2023-10-08 11:11:51 +0300

    Create LocationTest

---
+++ tests/Feature/LocationTest.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use App\Models\Location;
+use App\Models\Warehouse;
+use Inertia\Testing\AssertableInertia as Assert;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class LocationTest extends TestCase
+{
+    use RefreshDatabase;
+    /**
+     * A basic example.
+     */
+    public function test_the_application_returns_a_redirect_response(): void
+    {
+        $response = $this->get('/');
+
+        $response->assertRedirect(route('locations.index'));
+
+        $response = $this->get('/test');
+
+        $response->assertRedirect(route('locations.index'));
+    }
+
+    public function test_open_locations_index_page(): void
+    {
+        $this->get(route('locations.index'))
+            ->assertOk()
+            ->assertSessionDoesntHaveErrors()
+            ->assertInertia(
+                fn ($assert) => $assert
+                    ->component('Locations/Index')
+                    ->has('locations.data', 10)
+                    ->has(
+                        'locations.data.0',
+                        fn (Assert $assert) => $assert
+                            ->has('period_start_iso')
+                            ->has('period_end_iso')
+                            ->has('product.name')
+                            ->has('product.color.name')
+                            ->has('product.color.hex')
+                            ->has('warehouse.name')
+                            ->etc()
+                    )
+                    ->has('warehouses', Warehouse::count())
+                    ->has(
+                        'warehouses.0',
+                        fn (Assert $assert) => $assert
+                            ->has('id')
+                            ->has('name')
+                    )
+                    ->has('now')
+            );
+    }
+
+    public function test_open_locations_index_page_with_parameters(): void
+    {
+        $location = Location::first();
+        $productName = $location->product->name;
+        $warehouseId = $location->warehouse->id;
+
+        $this->get(
+            route('locations.index'),
+            [
+                'page' => 1,
+                'search' => $productName,
+                'warehouses' => [$warehouseId]
+            ]
+        )
+            ->assertOk()
+            ->assertSessionDoesntHaveErrors()
+            ->assertInertia(
+                fn (Assert $assert) => $assert
+                    ->component('Locations/Index')
+                    ->has('locations.data.0')
+                    ->has('warehouses')
+                    ->has('now')
+            );
+    }
+}
